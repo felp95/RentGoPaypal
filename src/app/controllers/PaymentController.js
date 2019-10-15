055294b98d0101d20e41293299af55c511df8ed6
felp95 <felp95@gmail.com>
2019-10-15 17:41:50 -0300

    finalizado o pagamento da corrida via paypal

--- src/app/controllers/PaymentController.js
+++ src/app/controllers/PaymentController.js
@@ -1,6 +1,5 @@
 const paypal = require('paypal-rest-sdk');
-const axios = require('axios')
-const qs = require('qs')
+var request = require("request");
 
 paypal.configure({
     'mode': 'sandbox',
@@ -15,26 +14,23 @@ class PaymentController {
         const payerId = req.query.PayerID;
         const paymentId = req.query.paymentId;
 
-        GetToken().then((response) => {
-            //doidera(payerId, response.data.access_token);
-        });
+        let tokenInformation = await GetToken();
+        let token = JSON.parse(tokenInformation);
 
-        let paymentInformation = await ListProductByPayId();
-
+        let paymentInformation = await ListProductByPayId(paymentId, token.access_token);
         var objectValue = JSON.parse(paymentInformation);
-        console.log(objectValue.transactions);
 
         let execute_payment_json = {
             "payer_id": payerId,
             "transactions": [{
                 "amount": {
-                    "currency": "BRL",
-                    "total": "25.00"
+                    "currency": objectValue.transactions[0].amount.currency,
+                    "total": objectValue.transactions[0].amount.total
                 }
             }]
         };
 
-        paypal.payment.execute(paymentId, paymentInformation, function (error, payment) {
+        paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
             if (error) {
                 console.log(error.response);
                 throw error;
@@ -62,44 +58,38 @@ class PaymentController {
 }
 
 const GetToken = async () =>  {
-    const url = "https://api.sandbox.paypal.com/v1/oauth2/token";
-
-    const data = {
-        grant_type: 'client_credentials'
-    };
-
-    const auth = {
-        username: 'AfPNQ1vJBY5ApfIBeS776NHjErcWozrZK43qIsmr8mAwC8a-ltpmYbP3nePxi-IpXGlqa5Kf1McMtLaR',
-        password: 'EDUmxcQPp1PcxLDzRrR81PuoqXi3c-7LalRvbgrHGfbaQGLusChy4VUE4QzkRVnb6FN2_UtZO0_LQ_LX'
-    };
-
-    const options = {
-        method: 'post',
-        headers: {
-            'content-type': 'application/x-www-form-urlencoded',
-            'Access-Control-Allow-Credentials': true
+    var options = { 
+        method: 'POST',
+        url: 'https://api.sandbox.paypal.com/v1/oauth2/token',
+        headers: {   
+            Host: 'api.sandbox.paypal.com',
+            Authorization: 'Basic QWZQTlExdkpCWTVBcGZJQmVTNzc2TkhqRXJjV296clpLNDNxSXNtcjhtQXdDOGEtbHRwbVliUDNuZVB4aS1JcFhHbHFhNUtmMU1jTXRMYVI6RURVbXhjUVBwMVBjeExEelJyUjgxUHVvcVhpM2MtN0xhbFJ2YmdySEdmYmFRR0x1c0NoeTRWVUU0UXprUlZuYjZGTjJfVXRaTzBfTFFfTFg=',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Accept-Language': 'en_US',
+            Accept: 'application/json' 
         },
-        data: qs.stringify(data),
-        auth: auth,
-        url,
+        form: { 
+            grant_type: 'client_credentials' 
+        } 
     };
 
-     return await axios(options)
+    return new Promise(function (resolve, reject) {
+        request(options, function (error, response, body) {
+            if (error) throw new Error(error);
+            return resolve(body);
+        });
+    });
 }
 
-const ListProductByPayId = async (payId, token) => {
-
-    
-    var request = require("request");
-
+const ListProductByPayId = async (paymentId, token) => {
     var options = { method: 'GET',
-      url: 'https://api.sandbox.paypal.com/v1/payments/payment/PAYID-LWSQGVI3MX64531ER789714H',
+      url: `https://api.sandbox.paypal.com/v1/payments/payment/${paymentId}`,
       headers: 
        { Connection: 'keep-alive',
          'Accept-Encoding': 'gzip, deflate',
          Host: 'api.sandbox.paypal.com',
          Accept: '*/*',
-         Authorization: 'Bearer A21AAGGHOEmvFEaUuAr8WipSp3C52w1cXHUSDbCoLih9NV_3U-KeWWviIjZZjQnNbmu7uBozfe4no-8R9EEX0hDp25sqqD6GA' } 
+         Authorization: `Bearer ${token}`} 
         };
         
         return new Promise(function (resolve, reject) {
